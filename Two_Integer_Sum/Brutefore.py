976402af5a1bfc939dae7346a7e725e8db206864
krizhnaa <krizhnaprem@gmail.com>
2024-06-13 19:18:30 +0530

    3.1

---
+++ Two_Integer_Sum/Brutefore.py
@@ -0,0 +1,15 @@
+
+nums = [2, 1, 5, 3]
+target = 7
+
+def twoSum(nums, target):
+    for i in range(len(nums) - 1):
+        for j in range(len(nums) - 1):
+            j+=1
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return "no result"
+
+
+result = twoSum(nums, target)
+print(result)
