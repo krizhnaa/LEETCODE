0f261234432f3907bef8613943ca63cef0855d7c
krizhnaa <krizhnaprem@gmail.com>
2024-06-13 12:52:33 +0530

    2.1

---
+++ Anagram/sort_both_&_compare.py
@@ -0,0 +1,18 @@
+
+# Also can use hashmap, which keeps the character of each string with their occurences
+
+s = "carrace"
+t = "raceca"
+
+def isAnagram(s, t):
+    sorted_s, sorted_t = ''.join(sorted(s)), ''.join(sorted(t))
+
+    if sorted_s == sorted_t:
+        return True
+    else:
+        return False
+
+result = isAnagram(s, t)
+print(result)
+
+
