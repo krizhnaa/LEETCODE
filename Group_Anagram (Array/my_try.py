afa1b0ef6433dd8c06713a3cc4c322a20c2fe71a
krizhnaa <krizhnaprem@gmail.com>
2024-06-17 08:39:20 +0530

    6.1 better

--- Group_Anagram (Array/my_try.py
+++ Group_Anagram (Array/my_try.py
@@ -1,6 +1,5 @@
 strs = ["act","pots","tops","cat","stop","hat"]
 
-
 def groupAnagram(strs):
 
     final_lst = []
@@ -26,15 +25,8 @@ def groupAnagram(strs):
                         strs.remove(i)
 
             final_lst.append(lst)
-        i = 0
-        while(i<len(final_lst)):
-            for ele in range(len(final_lst) - 1):
-                if len(final_lst[ele]) > len(final_lst[ele + 1]):
-                    temp = final_lst[ele]
-                    final_lst[ele] = final_lst[ele + 1]
-                    final_lst[ele + 1] = temp
-                i+=1
-    return final_lst
+        sorted_list = sorted(final_lst, key=len)
+    return sorted_list
 
 result = groupAnagram(strs)
 print(result)
