91e5ab6cc1661f9d4eb2bd8098779e2f05bb7d23
krizhnaa <krizhnaprem@gmail.com>
2024-06-18 11:00:01 +0530

    8

---
+++ Array/Prod_Array_not_itself/prefix&postfix.py
@@ -0,0 +1,21 @@
+
+nums = [1,2,4,6]
+
+
+def productExceptSelf(nums):
+    res = [1] * len(nums)
+
+    prefix = 1
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
+
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= postfix
+        postfix *= nums[i]
+
+    return res
+
+result = productExceptSelf(nums)
+print(result)
